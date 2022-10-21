32efbe852e8690232eb9ff09a6f3ef3d8e704ab3
Andrey Pryadko <andrey@pryadko.com>
2022-10-21 21:53:35 +0300

    Elements added

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCircle: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstCircle.layer.cornerRadius = 65
+        
     }
 
 
