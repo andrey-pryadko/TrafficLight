3605d7c95b814ed4396a96762fff68a201c8d2f8
Andrey Pryadko <andrey@pryadko.com>
2022-10-22 11:21:29 +0300

    work logic added + constraints fixes

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,29 +13,74 @@ class ViewController: UIViewController {
     @IBOutlet weak var yellowLightView: UIView!
     @IBOutlet weak var greenLightView: UIView!
     
+    @IBOutlet weak var mainButton: UIButton!
+    
+    private var lightStatus = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLights()
+        
+        mainButton.configuration = setupButton(with: "Start")
 
+    }
+    
+    @IBAction func mainButtonTapped() {
+        
+        mainButton.configuration = setupButton(with: "Next")
+        
+        switch lightStatus {
+        case 0:
+            redLightView.alpha = 1
+            lightStatus += 2
+        case 1:
+            shutdownAllLights()
+            redLightView.alpha = 1
+            lightStatus += 1
+        case 2:
+            shutdownAllLights()
+            yellowLightView.alpha = 1
+            lightStatus += 1
+        case 3:
+            shutdownAllLights()
+            greenLightView.alpha = 1
+            lightStatus = 1
+        default:
+            print("Error")
+        }
         
     }
     
-    private func setupLights() {
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = UIColor.blue
+        buttonConfiguration.title = title.uppercased()
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         
-        redLightView.layer.cornerRadius = 65
+        return buttonConfiguration
+    }
+    
+    private func setupLights() {
+        redLightView.layer.cornerRadius = 70
         redLightView.backgroundColor = UIColor.red
         redLightView.alpha = 0.3
         
-        yellowLightView.layer.cornerRadius = 65
+        yellowLightView.layer.cornerRadius = 70
         yellowLightView.backgroundColor = UIColor.yellow
         yellowLightView.alpha = 0.3
         
-        greenLightView.layer.cornerRadius = 65
+        greenLightView.layer.cornerRadius = 70
         greenLightView.backgroundColor = UIColor.green
         greenLightView.alpha = 0.3
-        
     }
-
+    
+    private func shutdownAllLights() {
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+    }
 
 }
 
